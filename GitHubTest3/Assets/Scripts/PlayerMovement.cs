6b48eaccec7943e4d25f74788f1e9527d491fa0d
CyberBinary <42885189+CyberBinary@users.noreply.github.com>
2018-09-01 15:43:02 -0700

    DLC Pack
    for only $5 you can unlock the rest of the game CONGRATS

--- GitHubTest3/Assets/Scripts/PlayerMovement.cs
+++ GitHubTest3/Assets/Scripts/PlayerMovement.cs
@@ -1,27 +1,70 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour {
+
+    public Text countText;
+    public Text winText;
 
     //This is a reference to the Rigidbody component called "rb"
     public Rigidbody rb;
 
-    public float forwardForce = 2000f;
+    public float forwardForce = 500f;
     public float sidewaysForce = 500f;
-	
-	// Update is called once per frame
+
+    private int count;
+
+    void Start ()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+
+
+    }
+    // Update is called once per frame
     // We marked it Fixed Update because Unity likes it with physics.
-	void FixedUpdate () {
-        //Add a forward force
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+    void FixedUpdate () {
+        //Add a forward force (currently disabled with //)
+        //rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
         if( Input.GetKey("d") )
         {
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
         }
 
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+
         if (Input.GetKey("a"))
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count >= 1)
+        {
+            winText.text = "You Win!!!!";
         }
     }
 }
---
+++ GitHubTest3/Assets/SpinScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinScript : MonoBehaviour {
+    public GameObject nameitwhateveryouwant;
+    public float spin = 50f;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.right * Random.Range(200, 400) * Time.deltaTime);
+        transform.Rotate(Vector3.back * Random.Range(40, 200) * Time.deltaTime);
+        transform.Rotate(Vector3.up * Random.Range(-200, 200) * Time.deltaTime);
+    }
+}
