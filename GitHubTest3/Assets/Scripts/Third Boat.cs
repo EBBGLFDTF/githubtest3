fb2aac01e90d29262ff90be481cf8018916a7721
PitEG <enriqueiigan@gmail.com>
2018-08-31 19:21:52 -0800

    Stress Test
    let's See

---
+++ GitHubTest3/Assets/Scripts/Third Boat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    //So How Hot does this thing get?
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
